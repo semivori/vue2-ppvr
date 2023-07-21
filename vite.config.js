a92fec89b56e755eddb1ebb0a926ef8d499d998f
semivori <semivoriii@gmail.com>
2023-07-21 16:38:54 +0300

    css

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     build: {
         lib: {
             // Could also be a dictionary or array of multiple entry points
-            entry: resolve(__dirname, 'lib/index.js'),
+            entry: [resolve(__dirname, 'lib/index.js')],
             name: 'vue2-ppvr',
             // the proper extensions will be added
             fileName: 'vue2-ppvr',
